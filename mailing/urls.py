c70af1a811f21d94a74a6370decca225a7d6ecaa
mihail <mihail.bashkatov@gmail.com>
2025-02-20 20:10:24 +0100

    Adding paths receiver, create, update, delete to mailing/urls.py

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import HomeTemplateView
+from .views import HomeTemplateView, ReceiverCreateView, ReceiverUpdateView, ReceiverDetailView, ReceiverDeleteView
 
 app_name = 'mailing'
 
 """ Registering URL adresses for mailing app"""
 urlpatterns = [
       path('home/', HomeTemplateView.as_view(), name='home_template'),
-
+      path('receiver/<int:pk>/', ReceiverDetailView.as_view(), name='receiver_detail'),
+      path('create/', ReceiverCreateView.as_view(), name='receiver_create'),
+      path('update/<int:pk>/', ReceiverUpdateView.as_view(), name='receiver_update'),
+      path('delete/<int:pk>/', ReceiverDeleteView.as_view(), name='receiver_delete'),
 ]
