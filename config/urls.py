ae35a6142916e783afb11ed62cadee6c2df5b7a6
mihail <mihail.bashkatov@gmail.com>
2025-02-21 14:40:16 +0100

    Registering app users in urlpatterns  config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mailing/', include('mailing.urls', namespace='mailing')),
-
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 # Cofiguration for media files
