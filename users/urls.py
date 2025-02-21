0776d07b719c11a4a896055ecd6e0ec418b6d8d3
mihail <mihail.bashkatov@gmail.com>
2025-02-21 18:16:02 +0100

    Adding path register_user in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from users.views import LoginUser
-
+from users.views import LoginUser, RegisterUser
 
 app_name = 'users'
 
@@ -10,5 +9,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterUser.as_view(), name='register_user'),
 
 ]
