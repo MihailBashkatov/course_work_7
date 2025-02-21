4f30daf07d7671119fe4199067e698eb2d733a59
mihail <mihail.bashkatov@gmail.com>
2025-02-21 17:11:10 +0100

    Adding class LoginUser in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+from users.forms import LoginUserForm
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Login'}
