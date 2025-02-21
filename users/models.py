db83398bf8419c8534337bc7fd6d2e2f12ecc158
mihail <mihail.bashkatov@gmail.com>
2025-02-21 14:55:45 +0100

    Createing model User in users/models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Add model User
+class User(AbstractUser):
+    """ Registering model User """
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = models.CharField(
+        max_length=30, blank=True, null=True, verbose_name="Phone number"
+    )
+    avatar = models.ImageField(
+        upload_to="users/images/%Y/%m/%d/",
+        default=None,
+        null=True,
+        blank=True,
+        verbose_name="Saved user image",
+    )
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["email",]
+
+    def __str__(self):
+        return self.email
