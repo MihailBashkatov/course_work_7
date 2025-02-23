d93b430aefb751046adade9e0c105c309f0266cc
mihail <mihail.bashkatov@gmail.com>
2025-02-23 12:37:52 +0100

    Adding functionality to send greeting mail ti user send_welcome_mail in RegisterView in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.views import LoginView
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import LoginUserForm, UserRegisterForm
 from users.models import User
 
@@ -19,3 +21,15 @@ class RegisterUser(CreateView):
     extra_context = {'title': 'Register'}
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        self.send_welcome_mail(user.email)  # send welcome email
+        return super().form_valid(form)
+
+    def send_welcome_mail(self, user_email):
+        subject = 'Welcome to Mailing App!'
+        message = 'Hello  thank you for joining our services!'
+        from_email = EMAIL_HOST_USER
+        recipient_list = [user_email,]
+        send_mail(subject, message, from_email, recipient_list)
