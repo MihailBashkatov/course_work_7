2bd6812f3934294de755f7243c9fdff2bfc1e50e
mihail <mihail.bashkatov@gmail.com>
2025-04-10 15:07:41 +0200

    Adding functions to get or set caches for mailing, messages, receivers in mailing/services.py

---
+++ mailing/services.py
@@ -0,0 +1,43 @@
+from django.core.cache import cache
+
+from config.settings import CACHE_ENABLED
+from mailing.models import Message, Mailing, Receiver
+
+
+def get_messages_from_cache(user):
+    """ Get data for the messages from cache. If no data in cache, cache messages"""
+    if not CACHE_ENABLED:
+        return Message.objects.filter(message_sender=user)
+    key = 'message_list'
+    messages = cache.get(key)
+    if messages is not None:
+        return messages
+    message_list = Message.objects.filter(message_sender=user)
+    cache.set(key, message_list)
+    return message_list
+
+
+def get_mailings_from_cache(user):
+    """ Get data for the mailings from cache. If no data in cache, cache mailings"""
+    if not CACHE_ENABLED:
+        return Mailing.objects.filter(mailing_sender=user)
+    key = 'mailing_list'
+    mailings = cache.get(key)
+    if mailings is not None:
+        return mailings
+    mailing_list = Mailing.objects.filter(mailing_sender=user)
+    cache.set(key, mailing_list)
+    return mailing_list
+
+
+def get_receivers_from_cache(user):
+    """ Get data for the receivers from cache. If no data in cache, cache receivers"""
+    if not CACHE_ENABLED:
+        return Receiver.objects.filter(receiver_adder=user)
+    key = 'receiver_list'
+    receivers = cache.get(key)
+    if receivers is not None:
+        return receivers
+    receiver_list = Receiver.objects.filter(receiver_adder=user)
+    cache.set(key, receiver_list)
+    return receiver_list
